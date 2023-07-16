13cc7cd404674503d7dfea683491f73e22a6939c
Wojciech Kesek <wojtekesek@gmail.com>
2023-07-16 10:58:29 +0200

    fix cmd

--- src/main/java/com/example/shapesspringboot/command/CommandLineBoot.java
+++ src/main/java/com/example/shapesspringboot/command/CommandLineBoot.java
@@ -10,10 +10,9 @@ import org.springframework.stereotype.Component;
 public class CommandLineBoot implements CommandLineRunner {
 
     @Autowired
-    Shape shape;
+    ShapeService service;
     @Override
     public void run(String... args) throws Exception {
-        ShapeService service = new ShapeService(shape);
         service.getShapeArea();
     }
 }
