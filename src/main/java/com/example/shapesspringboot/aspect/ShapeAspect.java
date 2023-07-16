238e6f31d60af7b5fe8604ae6b5fefde813c4ae2
Wojciech Kesek <wojtekesek@gmail.com>
2023-07-16 10:51:08 +0200

    add aspect

---
+++ src/main/java/com/example/shapesspringboot/aspect/ShapeAspect.java
@@ -0,0 +1,16 @@
+package com.example.shapesspringboot.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ShapeAspect {
+    @Around("execution(* getArea(..))")
+    public Object profile(ProceedingJoinPoint pjp) throws Throwable{
+        System.out.println("Dla figury " +  pjp.getSourceLocation().getWithinType().getSimpleName());
+        return pjp.proceed();
+    }
+}
