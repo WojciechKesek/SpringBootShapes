369a6bdd4273c6f7a450583d8e38895c28057f4d
Wojciech Kesek <wojtekesek@gmail.com>
2023-07-16 11:59:37 +0200

    injection by constructor

--- src/main/java/com/example/shapesspringboot/command/CommandLineBoot.java
+++ src/main/java/com/example/shapesspringboot/command/CommandLineBoot.java
@@ -1,16 +1,16 @@
 package com.example.shapesspringboot.command;
 
 
-import com.example.shapesspringboot.model.Shape;
 import com.example.shapesspringboot.service.ShapeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
+@RequiredArgsConstructor
 public class CommandLineBoot implements CommandLineRunner {
 
-    @Autowired
-    ShapeService service;
+
+    private final ShapeService service;
     @Override
     public void run(String... args) throws Exception {
         service.getShapeArea();
